2f263df22f89abf9ce5543267eafd5e69dcef0b5
Dhumra <dhumravarnaambre36@gmail.com>
2025-07-04 15:00:12 -0400

    Add cookie-parser for auth support

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth");
 const postRoutes = require("./routes/posts");
+const cookieParser = require("cookie-parser");
+
+
 
 require("dotenv").config();
 const app = express();
@@ -19,6 +22,9 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use(cookieParser());
+
+
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 
